bcfe9a1803944d756f1f7fd25c01176405fe2ed9
Alex <39219596+disruptfwd@users.noreply.github.com>
2018-05-12 18:03:22 -0700

    Updated clean_article function
    calling token_frequency_dic

--- clean_article.py
+++ clean_article.py
@@ -1,38 +1,33 @@
-
-# coding: utf-8
-
-# In[15]:
-
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import string
 
-# load data
-article = open('steve_smith_article.txt','r')
-text = article.read()
-file.close()
+def clean_article(articleName):
+    token_frequency_dic = {}
+    with open(articleName,'r') as f:
+        text = f.read()
 
-# split into words
-tokens = word_tokenize(text)
+        # split into words
+        tokens = word_tokenize(text)
 
-# convert to lower case
-tokens = [w.lower() for w in tokens]
+        # convert to lower case
+        tokens = [w.lower() for w in tokens]
 
-# remove punctuation from each word
-table = str.maketrans('', '', string.punctuation)
-stripped = [w.translate(table) for w in tokens]
+        # remove punctuation from each word
+        table = str.maketrans('', '', string.punctuation)
+        stripped = [w.translate(table) for w in tokens]
 
-# remove remaining tokens that are not alphabetic
-words = [word for word in stripped if word.isalpha()]
+        # remove remaining tokens that are not alphabetic
+        words = [word for word in stripped if word.isalpha()]
 
-# filter out stop words and sort
-stop_words = set(stopwords.words('english'))
-words = [w for w in words if not w in stop_words]
-words.sort()
+        # filter out stop words and sort
+        stop_words = set(stopwords.words('english'))
+        words = [w for w in words if not w in stop_words]
+        words.sort()
+        req = nltk.FreqDist(words)
+        for k,v in req.items():
+            token_frequency_dic[str(k)] = str(v)
+        return token_frequency_dic
 
-# print frequency distribution
-req = nltk.FreqDist(words)
-for k,v in req.items():
-    print(str(k) + ': ' + str(v))
-
+    f.close()
