6260c2ff2f5df18a07e1ae477e1402e010626e35
Alex <39219596+disruptfwd@users.noreply.github.com>
2018-05-15 17:43:11 -0700

    Updated Code with Named Entities Removal
    Removed named entities before changing text to lower case.

--- clean_article.py
+++ clean_article.py
@@ -1,17 +1,22 @@
 import nltk
 from nltk.tokenize import word_tokenize
+from nltk.tokenize import regexp_tokenize
 from nltk.corpus import stopwords
 import string
 
+# Remove named entities
+def ne_removal(text):
+    tokens = nltk.word_tokenize(text)
+    chunked = nltk.ne_chunk(nltk.pos_tag(tokens))
+    tokens = [leaf[0] for leaf in chunked if type(leaf) != nltk.Tree]
+    return tokens
+
+# Clean article
 def clean_article(articleName):
     token_frequency_dic = {}
     with open(articleName,'r') as f:
         text = f.read()
-
-        # split into words
-        tokens = word_tokenize(text)
-
-        # convert to lower case
+        tokens = ne_removal(text)
         tokens = [w.lower() for w in tokens]
 
         # remove punctuation from each word
@@ -20,14 +25,14 @@ def clean_article(articleName):
 
         # remove remaining tokens that are not alphabetic
         words = [word for word in stripped if word.isalpha()]
-
+        
         # filter out stop words and sort
         stop_words = set(stopwords.words('english'))
         words = [w for w in words if not w in stop_words]
         words.sort()
         req = nltk.FreqDist(words)
         for k,v in req.items():
-            token_frequency_dic[str(k)] = str(v)
+            token_frequency_dic[str(k)] = v
         return token_frequency_dic
 
     f.close()
